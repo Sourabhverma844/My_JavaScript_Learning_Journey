42e4401ee480a76ef26fdd7a9ccad068ea5a09af
Sourabh Verma <63900063+Sourabhverma844@users.noreply.github.com>
2024-12-02 18:36:22 +0000

    container config done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,20 @@
+value = 3
+Newvalue = -value
+console.log(Newvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Hello I am Sourabh Verma,"
+let str2 = " I am a Blockchain Developer"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); //agar string first he to sabhi string.
+console.log(1 + 2 + "2"); //agar number first he to sabhi number.
+
