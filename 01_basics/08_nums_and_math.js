d0f645b0b0027d6db36196d9b2a5c5ca6c9ab1ea
Sourabh Verma <63900063+Sourabhverma844@users.noreply.github.com>
2024-12-07 08:11:55 +0000

    container config done

--- 01_basics/08_nums_and_math.js
+++ 01_basics/08_nums_and_math.js
@@ -25,3 +25,24 @@ The "e+6" means "multiply by 10 raised to the power of 6.
 So, 
    1.00e+6 = 1.00 × 10⁶ = 1,000,000.
 In short, 1.00e+6 represents 1 million.*/
+
+/*---------------+++++ Maths +++++---------------*/
+console.log(Math)
+console.log(Math.abs(-0.4));
+console.log(Math.round(4.4)); // Give round-off value .5 se kam he islia 4
+console.log(Math.ceil(4.4)); // Give ceil value 4 se thoda bhi jyada to 5
+console.log(Math.round(4.6)); // Give round-off value .5 se jyada he islia 5
+console.log(Math.floor(4.6)); // Give floar value jabtak 4 ke points me he 4
+console.log(Math.min(0,1,2,3,4,5)); // Give min from array
+console.log(Math.max(0,1,2,3,4,5)); // Give max from array
+console.log(Math.random()); //Generates a random number between 0 (inclusive) and 1 (exclusive). means 0 a sakta he 1 kabhi nahi ayega
+
+const YourDigit = Math.floor(Math.random()*6+1)
+console.log(YourDigit);
+
+const MyDigit = Math.floor((Math.random()*10)+10);
+console.log(MyDigit);
+
+const min = 10
+const max = 20
+console.log(Math.random*(max-min+1));
