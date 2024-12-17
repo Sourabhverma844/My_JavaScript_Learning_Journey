45fc917837db70168a93c95b5e341edc228a8d9f
Sourabh Verma <sourabhverma844@gmail.com>
2024-12-17 17:13:47 +0530

    update

--- 05_iterations/05_for_each.js
+++ 05_iterations/05_for_each.js
@@ -66,4 +66,54 @@ ki hme koi condion check karna ho, ki uska name java ho to use retun kriye nahi 
 fir me , fir to hamara for each loop fail ho jaega.
 but for doin this we have diffrent approch
 */
-myNum = ["1]
+myNum = ["1", "2", "3", "4", "5", "6", "7", "8"]
+//vartype varname = arrayname.filter ( (callbackfunction) => condition)
+// jo bhi value condition ko satisfy kregi or condition true hogi only un values ko return kara jaega 
+const newNums = myNum.filter( (num) => num > 4)
+console.log(newNums);
+//yaha hamne ((num) => num > 4) parenthesis use kiya hua he to hame explicitly return nahi likhna pdega
+
+myNo = ["11", "12", "13", "14", "15", "16"] 
+const nNo = myNo.filter( (ank) => {
+    return ank>=12
+}
+)
+console.log(nNo);
+
+// Now try to use for each loop here
+const Salary = [ 3000000, 4000000, 5000000, 6000000 ]
+const mySalary = [];
+Salary.forEach( (Money) => {
+    if (Money >= 4000000) {
+        mySalary.push(Money)
+    }
+} );
+console.log(mySalary);
+
+const allBooks = [
+    { title:'Book One', genre: 'fiction', publish:'1981', edition:2010},
+    { title:'Book Two', genre: 'non-fiction', publish:'1982', edition:2011},
+    { title:'Book Three', genre: 'Drama', publish:'1983', edition:2012},
+    { title:'Book Four', genre: 'Science', publish:'1984', edition:2013},
+    { title:'Book Five', genre: 'Philosophy', publish:'1985', edition:2014},
+    { title:'Book Six', genre: 'Phycology', publish:'1986', edition:2015},
+    { title:'Book Seven', genre: 'Sociology', publish:'1987', edition:2016},
+    { title:'Book Eight', genre: 'Technical', publish:'1988', edition:2017},
+    { title:'Book Nine', genre: 'Trading', publish:'1989', edition:2018},
+    { title:'Book Ten', genre: 'Sales', publish:'1990', edition:2019},
+];
+let myBooks = allBooks.filter( (UserBooks) => UserBooks.genre === 'Sales' )
+console.log(myBooks);
+myBooks = allBooks.filter( (UserBooks) => UserBooks.genre === 'Technical' )
+console.log(myBooks);
+myBooks = allBooks.filter ((UserBook) => UserBook.title === 'Book Five' )
+console.log(myBooks)
+myBooks = allBooks.filter ( (NonExit) => NonExit.genre === 'Science' )
+console.log(myBooks)
+myBooks = allBooks.filter ( (UserBook) => UserBook.genre === 'Heroism')
+// if jo data humne manga he wo available nahi hua to ye hme ek empty array de dega output me 
+console.log(myBooks)
+myBooks = allBooks.filter ( (UserBook) => { return UserBook.edition >= 2017 && UserBook.genre === 'Technical'})
+console.log(myBooks)
+myBooks = allBooks.filter ( (UserBook) => { return UserBook.edition >= 2019 || UserBook.genre === 'Technical'})
+console.log(myBooks)
